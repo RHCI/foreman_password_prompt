51673d37dc3c559217eafaae563d389f5a9c0876
jesus m. rodriguez <jesusr@redhat.com>
2014-11-04 15:42:09 -0500

    debug logging

--- app/controllers/foreman_password_prompt/users_controller.rb
+++ app/controllers/foreman_password_prompt/users_controller.rb
@@ -2,6 +2,7 @@ module ForemanPasswordPrompt
 
   # Example: Plugin's UsersController inherits from Foreman's UsersController
   class UsersController < ::UsersController
+      include Foreman::Controller::UsersMixin
 
     # change layout if needed
     # layout 'foreman_password_prompt/layouts/new_layout'
@@ -11,20 +12,25 @@ module ForemanPasswordPrompt
       puts "New action called"
       puts params[:new_password]
       puts params[:confirm_password]
+      if User.current.nil?
+          puts "User is null"
+      elsif
+          puts User.current.login
+      end
 
       if params[:new_password] == params[:confirm_password]
           puts "Changing password"
+          puts User.current.password
+          puts User.current.password_hash
+          User.current.password= params[:new_password]
+          puts User.current.password
+          puts User.current.password_hash
       elsif
           puts "Passwords are not the same"
       end
 
-      # this breaks things
-      #@user = find_resource(:edit_users)
-      #if @user == nil
-      #    puts @user.login
-      #elsif
-      #    puts "User is null"
-      #end
+      User.current.save
+
     end
 
   end
