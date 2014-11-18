fc00ec28331fe531d40b4b6d5bb381e6ca8f9509
jesus m. rodriguez <jesusr@redhat.com>
2014-11-18 16:10:39 -0500

    avoid conflicts just in case :)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ::ApplicationController
 
-    before_filter :change_password
+    before_filter :should_change_password
 
     private
 
-    def change_password
+    def should_change_password
         puts "YYY We need to change passwords"
         redirect_to(:change_password)
     end
